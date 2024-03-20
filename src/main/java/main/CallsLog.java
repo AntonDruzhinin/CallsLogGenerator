4bc58e00a73dc67835b2469b9c7eb791e70735c1
Anton Druzhinin <druzhininantn@gmail.com>
2024-03-20 14:27:43 +0300

    -spring, +abonent generator

---
+++ src/main/java/main/CallsLog.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+
+public class CallsLog {
+    final String path = "/reports";
+    final Date STARTSOFREPORT = new Date(2023, 03, 20);
+    final Date FINISHOFREPORT = new Date(2024, 03, 20);
+    public static void main(String[] args) {
+        List<Long> abonents = new ArrayList<>();
+
+
+
+    }
+
+     public static List<Long> getAbonentList(int numOfAbonents){
+        List<Long> abonentList = new ArrayList<>();
+        for(int i = 0; i <= numOfAbonents; i++){
+            Random rn = new Random();
+            long number= rn.nextLong(79999999999L - 79000000000L  + 1) + 79000000000L;
+            abonentList.add(number);
+            System.out.println(number);
+        }
+        return abonentList;
+    }
+}
